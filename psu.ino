af2b1e30c1f5dc1fb7164606ce6763d89816daf3
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2021-11-15 19:33:29 -0300

    First commit

---
+++ psu.ino
@@ -0,0 +1,61 @@
+#include <LowPower.h>
+
+const byte PIN_IGNITION = 0;
+const byte PIN_POWER = 1;
+const byte PIN_FEEDBACK = 2;
+const byte PIN_NC1 = 3;
+const byte PIN_NC2 = 4;
+
+const byte STATE_OFF = 0;
+const byte STATE_POWERING_UP = 1;
+const byte STATE_ON = 2;
+const byte STATE_SHUTTING_DOWN = 3;
+
+void setup() {
+  pinMode(PIN_IGNITION, INPUT_PULLUP);
+  pinMode(PIN_POWER, OUTPUT);
+  pinMode(PIN_FEEDBACK, INPUT_PULLUP);
+  pinMode(PIN_NC1, INPUT_PULLUP);
+  pinMode(PIN_NC2, INPUT_PULLUP);
+}
+
+void loop() {
+  LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+
+  static byte currentState = STATE_OFF;
+
+  boolean ignition = digitalRead(PIN_IGNITION) == LOW;
+  boolean feedback = digitalRead(PIN_FEEDBACK) == LOW;
+
+  switch (currentState) {
+    case STATE_OFF:
+      if (ignition) {
+        digitalWrite(PIN_POWER, HIGH);
+        currentState = STATE_POWERING_UP;
+      }
+
+      break;
+
+    case STATE_POWERING_UP:
+      if (feedback) {
+        currentState = STATE_ON;
+      }
+
+      break;
+
+    case STATE_ON:
+      if (!ignition) {
+        digitalWrite(PIN_POWER, LOW);
+        currentState = STATE_SHUTTING_DOWN;
+      }
+
+      break;
+
+    case STATE_SHUTTING_DOWN:
+      if (!feedback) {
+        currentState = STATE_OFF;
+      }
+
+      break;
+  }
+}
