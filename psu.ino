1badf77f5925e5be0642158df94d3f3cc9c1e61e
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2021-11-23 20:34:00 -0300

    Implement a relay to turn off buck converter

--- psu.ino
+++ psu.ino
@@ -1,10 +1,12 @@
+// Original: https://github.com/rocketscream/Low-Power
+// ATtiny85: https://github.com/rocketscream/Low-Power/pull/92
 #include <LowPower.h>
 
 const byte PIN_IGNITION = 0;
-const byte PIN_POWER = 1;
-const byte PIN_FEEDBACK = 2;
-const byte PIN_NC1 = 3;
-const byte PIN_NC2 = 4;
+const byte PIN_RELAY = 1;
+const byte PIN_POWER = 2;
+const byte PIN_FEEDBACK = 3;
+const byte PIN_NC1 = 4;
 
 const byte STATE_OFF = 0;
 const byte STATE_POWERING_UP = 1;
@@ -13,14 +15,14 @@ const byte STATE_SHUTTING_DOWN = 3;
 
 void setup() {
   pinMode(PIN_IGNITION, INPUT_PULLUP);
+  pinMode(PIN_RELAY, OUTPUT);
   pinMode(PIN_POWER, OUTPUT);
   pinMode(PIN_FEEDBACK, INPUT_PULLUP);
   pinMode(PIN_NC1, INPUT_PULLUP);
-  pinMode(PIN_NC2, INPUT_PULLUP);
 }
 
 void loop() {
-  LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+  LowPower.powerDown(SLEEP_2S, ADC_OFF, BOD_OFF);
 
   static byte currentState = STATE_OFF;
 
@@ -30,6 +32,7 @@ void loop() {
   switch (currentState) {
     case STATE_OFF:
       if (ignition) {
+        digitalWrite(PIN_RELAY, HIGH);
         digitalWrite(PIN_POWER, HIGH);
         currentState = STATE_POWERING_UP;
       }
@@ -53,6 +56,7 @@ void loop() {
 
     case STATE_SHUTTING_DOWN:
       if (!feedback) {
+        digitalWrite(PIN_RELAY, LOW);
         currentState = STATE_OFF;
       }
 
